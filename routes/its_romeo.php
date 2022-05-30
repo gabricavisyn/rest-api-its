ead196e0b79c3aa4ffb6252f61b8f422cdb51b82
Franz <francesco.romeo-work@libero.it>
2022-05-30 11:44:04 +0200

    Fixed api url

--- routes/its_romeo.php
+++ routes/its_romeo.php
@@ -8,6 +8,12 @@ Route::get('/rickroll', function(Request $request){
     return response("https://www.youtube.com/watch?v=dQw4w9WgXcQ");
 });
 
-Route::get('/salve', function(Request $request){
-    return response("<h1>Ciao mondo</h1> <br> <a href='/https://www.youtube.com/watch?v=dQw4w9WgXcQ' style="color:red font-size: 34pt">Click(?)</a>");
+Route::get('/home1', function(Request $request){
+    return response("<h1>Ciao mondo</h1>
+    <br>
+    <a
+      href='/https://www.youtube.com/watch?v=dQw4w9WgXcQ'
+      style="color:red font-size: 34pt">
+      Click(?)
+   </a>");
 });
