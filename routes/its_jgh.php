c94a51d2a1f4a6f8e26c02453a1338cc7e28df46
Soufian Jghalef <icts21-23.336@A4-24.local>
2022-05-30 12:04:20 +0200

    Aggiunta funzione redirect

--- routes/its_jgh.php
+++ routes/its_jgh.php
@@ -5,3 +5,7 @@
     Route::get('/Saluta', function(Request $request){
         return response("<h1>Ciao ".$nome."!</h1>");
     });
+
+    Route::get('/redirect', function(Request $request){
+        return redirect('https://google.com');
+    });
