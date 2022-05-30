662b399b9bd8a935b5bcd39bd399724b6cacca3d
Alessandro <alessandro.scisci@edu.itspiemonte.it>
2022-05-30 12:07:55 +0200

    added test redirect

--- routes/its_sci.php
+++ routes/its_sci.php
@@ -6,3 +6,9 @@ use Illuminate\Support\Facades\Route;
 Route::get('/salve', function(Request $request){
     return response("<h1>Evviva la richiesta funziona!</h1>");
 });
+
+
+
+Route::get('/redirect', function(Request $request){
+    return response()->redirectTo('https://it.wikipedia.org/');
+});
